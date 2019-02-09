a6f9911606809085d29408c292524c6177cf55d8
Gilles Lima <gillestoim@gmail.com>
2019-02-09 00:31:29 -0200

    generate links array; scraping now picked up all the registers

--- src/service/scraping/index.js
+++ src/service/scraping/index.js
@@ -11,31 +11,66 @@ const scrape = async (name) => {
     await page.click('a[id=botaoBuscaFiltros]');
     await page.waitForSelector('div.resultado');
 
-    const paginated = await page.$$eval('a[data-role=paginacao]', links => links.map(link => link.href));
-    // console.log(paginated.length)
-    // console.log(paginated)
+    const numRegisters = await page.$eval('div.tit_form b', element => element.innerText);
+    const referenceURL = await page.$$eval('a[data-role=paginacao]', links => links.map(link => link.href)[0]);
+
+    // console.log(numRegisters);
+    // console.log(referenceURL);
     
-    for (let i = 0; i < paginated.length/2; i++) {
-        await page.goto(paginated[i], { waitUntil: "load" });
+    if (referenceURL) {
+        const targetL = await _generateLinks(referenceURL, ~~numRegisters);
+        // console.log(targetL);
+
+        if (~~numRegisters <= 100) {
+
+            await page.goto(referenceURL.replace('registros=0;10', `registros=0;${numRegisters}`), { waitUntil: "load" });
+            data.push(await _content(page));
         
-        const persons = await page.evaluate(() => {
-            const nodeList = document.body.querySelectorAll('li');
-            return Array.from(nodeList).map(item => ({
-                name: item.querySelector('a').textContent,
-                country: item.contains(item.querySelector('img')) ? 
-                    item.querySelector('img').getAttribute('alt') : '',
-                details: item.querySelector('a').getAttribute('href'),
-                resume: item.innerText
-            }));            
-        });
+        } else {
+            
+            for (let i = 0; i < targetL.length; i++) {
+                await page.goto(targetL[i], { waitUntil: "load" });
+                data.push(await _content(page)); 
+            };
 
-        data.push(persons);
+        }
 
+    } else {
+        data.push(await _content(page))
     }
 
     browser.close()
 
     return data;
+}
+
+
+_content = async (context) => {
+        
+    return await context.evaluate(() => {
+        const nodeList = document.body.querySelectorAll('li');
+        return Array.from(nodeList).map(item => ({
+            name: item.querySelector('a').textContent,
+            country: item.contains(item.querySelector('img')) ? 
+                item.querySelector('img').getAttribute('alt') : '',
+            details: item.querySelector('a').getAttribute('href'),
+            resume: item.innerText
+        }));            
+    });
+
+}
+
+_generateLinks = (referenceURL, numRegisters) => {
+    let result = [];
+
+    for (let x = 0; x < numRegisters; x+=10) {
+        // console.log('x: ', x);
+        x > 0 ? 
+        result.push(referenceURL.replace(`registros=0;10`, `registros=${ x };10`)) :
+        result.push(referenceURL);
+    };
+
+    return result;
 }
 
 module.exports = scrape;
