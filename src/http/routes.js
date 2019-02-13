597abfc2fe00c4f985500365ff9611f9e911e460
Gilles Lima <gillestoim@gmail.com>
2019-02-12 22:51:17 -0200

    created routes file

---
+++ src/http/routes.js
@@ -0,0 +1,27 @@
+const { scrape, scrapeNextPage } = require('../service/scraping');
+
+const routes = (app) => {
+    app.post('/api/v1/search', async (req, res) => {
+        const { search } = req.body;
+        try {
+            res.json(await scrape(search)
+                .then((res) => res)
+                .catch(err => console.log(err)));
+        } catch (error) {
+            res.json(404, error);
+        }
+    });
+    
+    app.post('/api/v1/next-page', async (req, res) => {
+        const { url } = req.body;
+        try {
+            res.json(await scrapeNextPage(url)
+                .then((res) => res)
+                .catch(err => console.log(err)));
+        } catch (error) {
+            res.json(404, error);
+        }
+    });
+}
+
+module.exports = routes;
