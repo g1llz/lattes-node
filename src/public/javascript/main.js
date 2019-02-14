b1f7b90c03fd53d1b92c7a5f86fdc66b0fb18624
Gilles Lima <gillestoim@gmail.com>
2019-02-14 21:49:19 -0200

    navigation to next page; fixed some methods

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     button.onclick = async (e) => {
         e.preventDefault();
 
-        document.getElementById('dloader').style.display = 'inline-block';
-        document.getElementById('content').style.opacity = '0.2';
+        _loader().show();
+        _toTop();
 
         let searchFor = document.querySelector('input[name=chooseOption1]:checked').value;
         let whichBase = document.querySelector('input[name=chooseOption2]:checked').value;
@@ -40,23 +40,39 @@ const _findInLattes = (searchObj) => {
         .catch(err => console.log(err));
 }
 
+const _accessNextPage = (url) => {
+    const headers = { 
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify({ url: url }) 
+    };
+
+    return fetch('/api/v1/next-page', headers)
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err));
+}
+
 const _contentRender = (resultObj) => {
     const reciver = document.getElementById('content');
+
+    resultObj.pages && _paginatorRender(resultObj.pages);
+
     const content = `
         <h5>Resultados</h5>
         <hr>
         <ul class="list-unstyled">
-        ${ resultObj.persons.map((person, index) => 
+        ${ resultObj.persons.map((person) => 
             `<li class="mb-2">
                 <div class="card">
                     <div class="card-header d-flex 
                     justify-content-between align-itens-center">
                         <span>
                             <strong>${ person.name }</strong> 
+                        </span>
+                        <span>
                             <small class="ml-2">${ person.country }</small>
                         </span>
-                        <span><strong>#${ index+1 }</strong></span>
-                        
                     </div>
                     <div class="card-body">
                         <ul class="list-unstyled">
@@ -73,6 +89,58 @@ const _contentRender = (resultObj) => {
 
     reciver.innerHTML = content;
 
-    document.getElementById('content').style.opacity = '1';
-    document.getElementById('dloader').style.display = 'none';
+    _loader().hide();
+}
+
+const _paginatorRender = (pages) => {
+    const reciver = document.getElementById('paginator');
+    const content = `
+        <ul class="d-flex justify-content-center flex-wrap list-unstyled">
+            ${ pages.map((page, index) => 
+                `<li>
+                    <button type="button" class="btn btn-link btn-sm btn-next-page ${ index == 0 ? 'active' : '' }" data-target-url="${ page }">
+                        ${ index + 1 }
+                    </button>
+                </li>`).join('') }
+        </ul>
+    `;
+
+    reciver.innerHTML = content;
+
+    const links = reciver.querySelectorAll('[data-target-url]');
+
+    Array.prototype.filter.call(links, function(link) {
+
+        link.addEventListener('click', async function (e) { 
+            e.preventDefault();
+
+            links.forEach(button => button.classList.remove('active'));
+
+            this.classList.add('active');
+
+            _loader().show();
+            _toTop();
+
+            let result = await _accessNextPage(link.dataset.targetUrl);
+            _contentRender(result);
+        })
+    })
+}
+
+const _loader = () => {
+    return {
+        show: () => {
+            document.getElementById('dloader').style.display = 'inline-block';
+            document.getElementById('content').style.opacity = '0.2';
+        },
+        hide: () => {
+            document.getElementById('content').style.opacity = '1';
+            document.getElementById('dloader').style.display = 'none';
+        }
+    }
+}
+
+const _toTop = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
