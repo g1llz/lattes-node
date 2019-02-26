5b1e3485c55e197cea1e7a4dda56f5e115ff0163
Gilles Lima <gillestoim@gmail.com>
2019-02-25 22:38:36 -0300

    server update

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('../http/routes');
-const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, '../public')));
-app.set('views', path.join(__dirname, '../public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
 routes(app);
 
